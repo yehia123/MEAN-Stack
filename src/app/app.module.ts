c31128d4941ca1db0fc56091f45dec926b011566
Yehia Q <yehiaqtaish@gmail.com>
2018-07-05 10:30:26 -0700

    Added Image Preview still need to connect to database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {
   MatInputModule,
@@ -70,7 +70,7 @@ export function provideConfig() {
     MatDividerModule,
     MatFormFieldModule,
     MatSelectModule,
-    FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatExpansionModule,
     HttpClientModule,
--- src/app/posts/create/create-post.component.ts
+++ src/app/posts/create/create-post.component.ts
@@ -1,8 +1,9 @@
-import { Component, Inject, Injectable } from '@angular/core';
+import { Component, Inject, Injectable, OnInit } from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
 
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostService } from '../posts.service';
+import { mimeType } from './mime-type.validator';
 
 /* Component which creates a rental post
 * onAddPost() currently uses two way binding with enteredValue
@@ -16,6 +17,8 @@ import { PostService } from '../posts.service';
 export class CreatePostComponent {
   enteredTitle = '';
   enteredDesc = '';
+  isLoading = false;
+  imagePreview: string;
   /** The Emitter along with the output is what connects this
    * file to the outside and parent component such as app.comp.html/ts
    * generic type Post <> similar to Java
@@ -24,18 +27,52 @@ export class CreatePostComponent {
   shippingSelection = '';
   renLength = '';
 
+
+
   constructor(
     public postsService: PostService
   ) {}
+  /** Control form using Reactive Forms
+   * async Validator to check if it is the right type of file
+   */
+  form = new FormGroup({
+    title: new FormControl(null, {
+      validators: [Validators.required]
+    }),
+    content: new FormControl(null, {
+      validators: [Validators.required]
+    }),
+    image: new FormControl(null, {
+      validators:
+       [Validators.required],
+       asyncValidators: [mimeType]
+    })
+  });
+  /** Method in intake the uplaoded image using HTMLInputElement method which
+   * gives us access to the files[] array.
+   * in this case only the first image uploaded is chosen
+   * patchValue() target single control
+   */
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+   /** Transform image to url for preview */
+   const reader = new FileReader();
+   reader.onload = () => {
+     this.imagePreview = reader.result;
+   };
+   reader.readAsDataURL(file);
+  }
 
 
-  onAddPost(form: NgForm) {
+  onAddPost() {
     /** exits the method and the post does not get added */
-    if (form.invalid) {
+    if (this.form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
-    form.resetForm();
+    this.postsService.addPost(this.form.value.title, this.form.value.content);
+    this.form.reset();
   }
 
 }
